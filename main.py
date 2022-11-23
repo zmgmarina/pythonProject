26f42d165567d25cec977a889750b22f91766741
Marina <zmgmarina@mail.ru>
2022-11-24 01:28:16 +0500

    add seminar_1

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+a = 0
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# if a > 0:
+#     print('положительное')
+# elif a < 0:
+#     print('отрицательное')
+# else:
+#     print('Число равно', 0)
+#
+#
+# # циклы
+# for i in range(5):
+#     print('mama')
+#
+# # или так
+# i = 0
+# while i < 5:
+#     print('mama')
+#     i +=1
+
+# n = 567
+# print(n // 10)
+# print(n % 10)
+
+# списки
+
+# sp = list()
+# sp = []
+# sp.append(5) # заполнение списка
+# sp.append(8)
+# sp.append(6)
+# sp.append(1)
+# print(sp)
+
+# обращение к элементам списка
+sp = [1,2,6,8,0]
+# print(sp[2])
+# print(sp[-1])
+# print(sp[-2])
+
+# # перебор списка циклом по индексам (если будут меняться значения)
+# for i in range(0, len(sp)):
+#     print(sp[i], end=' ')
+# print()
+# # перебор списка циклом по элементам (если просто распечатать)
+# for el in sp:
+#     print(el, end=' ')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
