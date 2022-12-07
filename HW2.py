6e82b261117cfd3a57ca3f2913d2d46f49c35645
Marina <zmgmarina@mail.ru>
2022-12-07 23:38:36 +0500

    HW_2_add

--- HW2.py
+++ HW2.py
@@ -1,57 +1,58 @@
-# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-# Пример:
-# - 6782 -> 23
-# - 0,56 -> 11
-
-
-num = float(input("Введите число: "))
-if num < 0:
-    num = float(num) * (-1)
-
-while num != int(num):
-    num *= 10
-
-sum = 0
-while num > 0:
-    if num != '.':
-        sum += num % 10
-        num //= 10
-print(int(sum))
-
-
-
-
-# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-
-# Пример:
-
-# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
-
-N = int(input('Введите число n: '))
-sp = []
-number = 1
-
-for i in range(N):
-    i += 1
-    number *= i
-    sp.append(number)
-print(sp, end=",")
-
-
-
-# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-
-N = int(input('Введите число n: '))
-sp = list()
-
-for i in range(1, N):
-    sp.append(i)
-print(sp)
-
-s = 0
-for i in sp:
-    s += (1 + 1 / i) ** i
-print(round(s, 2))
+# # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# # Пример:
+# # - 6782 -> 23
+# # - 0,56 -> 11
+#
+#
+# a = input("Введите число: ")
+# s = 0
+# for i in a:
+#     if i.isdigit(): # если i является числом, то его суммируем
+#         s += int(i)
+# print(s)
+#
+#
+#
+#
+# # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+#
+# # Пример:
+#
+# # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+#
+# N = int(input('Введите число n: '))
+# sp = []
+# number = 1
+#
+# for i in range(N):
+#     i += 1
+#     number *= i
+#     sp.append(number)
+# print(sp, end=",")
+#
+# # или такой способ
+#
+# n = int(input('Введите число  '))
+# p = 1
+# for i in range(1, n + 1):
+#     p = p * i
+#     print(p, end=' ')
+#
+# exit()
+#
+# # Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+#
+# N = int(input('Введите число n: '))
+# sp = list()
+#
+# for i in range(1, N):
+#     sp.append(i)
+# print(sp)
+#
+# s = 0
+# for i in sp:
+#     s += (1 + 1 / i) ** i
+# print(round(s, 2))
 
 
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных
@@ -59,8 +60,8 @@ print(round(s, 2))
 
 
 data = open('file.txt', 'w')
+data.write('1\n')
 data.write('2\n')
-data.write('3\n')
 data.close()
 
 N = int(input('Введите число n: '))
@@ -70,8 +71,11 @@ for i in range(-N, N+1):
     sp.append(i)
 print(sp)
 
-ind = open('file.txt', 'r')
-mul = sp[int(ind.readline())] * sp[int(ind.readline(2))]
+with open('file.txt', 'r') as f:
+    index = f.readlines()
+mul = 1
+for i in index:
+    mul *= sp[int(i)]
 print(mul)
 
 
